52803a6872267173f98840127d4f3870d51cb3c8
cycyc2333 <2516803593@qq.com>
2023-03-06 23:11:58 +0800

    0306 update

--- 2303/code/01.c
+++ 2303/code/01.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-#define PI 3.1415926
+#define PI 3.1415926535
+//如果pi不精确到小数点后十位会有一个测试点通不过，垃圾题/doge
 
 void ball();
 void cylinder();
@@ -7,16 +8,17 @@ void cone();
 
 int main()
 {
-    int command;
+    char command;
     while(1){
         printf("1-Ball\n2-Cylinder\n3-Cone\nother-Exit\nPlease enter your command:\n");
-        scanf("%d",&command);
+        command = getchar();
         switch (command) {
-        case 1: ball();break;
-        case 2: cylinder();break;;
-        case 3: cone();break;
+        case '1': ball();break;
+        case '2': cylinder();break;;
+        case '3': cone();break;
         default:return 0;
     }
+    getchar();
     }
     return 0;
 }
